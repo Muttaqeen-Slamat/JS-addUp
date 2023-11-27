c648f3af6763c7699d4fcaa9291572f09336f73c
Muttaqeen-Slamat <muttaqeens@gmail.com>
2023-11-27 13:04:05 +0200

    patch 2

--- script/code.js
+++ script/code.js
@@ -1,10 +1,10 @@
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 function addUp(){
-    let sum = 0
+    let add = 0
     for( let i=0; i < numbers.length ; i++){
-        sum += numbers[i]
+        add += numbers[i]
     }
-    console.log(sum);
+    console.log(add);
 }
 addUp()
