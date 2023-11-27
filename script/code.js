20cdd871f46ef0b2f39510b55d28c287a25a5ae6
Muttaqeen-Slamat <muttaqeens@gmail.com>
2023-11-27 13:05:16 +0200

    patch 3

--- script/code.js
+++ script/code.js
@@ -1,7 +1,10 @@
+//global array
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+//function
 function addUp(){
     let add = 0
+    //method
     for( let i=0; i < numbers.length ; i++){
         add += numbers[i]
     }
